effd79cf38e7abf33e8c45d98531c8a37823f0d5
singa301 <singa301@mcmaster.ca>
2024-02-12 19:15:13 -0500

    added kalman filter logic using dummy values, changed variable names

--- Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
+++ Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
@@ -13,11 +13,17 @@
 
 Adafruit_MPU6050 imu;
 double currentAngle = 0.0;
+double Q_angle = 0.001; 
+double Q_gyro = 0.003; 
+double R_measure = 0.03; 
 
+double angle = 0.0; 
+double bias = 0.0; 
+double P[2][2] = {{1, 0}, {0, 1}}; 
 
 void setup() {
-    Serial.begin(9600);
-    Wire.begin();  
+    Serial.begin(9600);  
+  
     imu.setGyroRange(MPU6050_RANGE_5_DEG);
 
     imu.setAccelerometerRange(MPU6050_RANGE_1G);
@@ -33,10 +39,44 @@ void loop() {
     imu.getEvent(&accelerometerEvent, &gyroscopeEvent);
     double rawAngle = atan2(-accelerometerEvent.acceleration.y, -accelerometerEvent.acceleration.x) * RAD_TO_DEG;
 
+    double dt = 0.1;
+    KalmanFilter(rawAngle, gyroscopeEvent.gyro.z, dt);  
+
     if (currentAngle == 0.0) {
       currentAngle = rawAngle;
     }
     //if (abs(currentAngle - rawAngle) > 5.0) {
     //} //should run PID logic here in case the angle is veering more than MPU6050_RANGE_5_DEG
     delay(100);
+}
+
+void KalmanFilter(double newAngle, double newRate, double dt) {
+    // Prediction update
+    double rate = newRate - bias;
+    angle += dt * rate;
+
+    // Update estimation error covariance - Project the error covariance ahead
+    P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
+    P[0][1] -= dt * P[1][1];
+    P[1][0] -= dt * P[1][1];
+    P[1][1] += Q_gyro * dt;
+
+    // Measurement update
+    double S = P[0][0] + R_measure; // Estimate error
+    double K[2]; // Kalman gain
+    K[0] = P[0][0] / S;
+    K[1] = P[1][0] / S;
+
+    double y = newAngle - angle; // Angle difference
+    angle += K[0] * y; // Update angle estimation
+    bias += K[1] * y; // Update bias estimation
+
+    // Update the error covariance matrix
+    double P00_temp = P[0][0];
+    double P01_temp = P[0][1];
+
+    P[0][0] -= K[0] * P00_temp;
+    P[0][1] -= K[0] * P01_temp;
+    P[1][0] -= K[1] * P00_temp;
+    P[1][1] -= K[1] * P01_temp;
 }
