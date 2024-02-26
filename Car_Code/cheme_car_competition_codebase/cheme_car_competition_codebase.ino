a9855b79712f211ac4d9ffad60ea58c229cda0b0
singa301 <singa301@mcmaster.ca>
2024-02-26 17:37:07 -0500

    added a variable storing the filtered angle

--- Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
+++ Car_Code/cheme_car_competition_codebase/cheme_car_competition_codebase.ino
@@ -1,9 +1,3 @@
-// TODO:
-// Temeperature Sensor - Needs to be initialized, periodically poll sensor, store temperature in variable, if reaches threshold of 3 deg increase from what it first read, stop the drive motors, look into kalman filters for reducing sensor noise.
-// IMU - Needs to be initialized, periodically poll sensor, store Z-axis angle in variable, take initial value as 0 deg, if veering off by more than 5 deg, adjust using a PID loop to get back on track, look into kalman filters for reducing sensor noise.
-// Drive Motors - Need to be initialized, take input from the IMU as necessary and move, otherwise start off moving straight, and keep moving straight, move at about 80% max speed for now, we will change as needed.
-// Stir Bar Motor - Needs to be initialized, just constantly turn at 80% speed for now, we'll tune speed later.
-// Servo Motor - Needs to be initialized, turn 180 deg clockwise once at start to dump reactants, wait 1 sec to finish dumping, turn back 180 deg counter-clockwise to return to upright position, set speed to 100% for now, tune later.
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
@@ -12,7 +6,9 @@
 #define RAD_TO_DEG 180.0 / PI
 
 Adafruit_MPU6050 imu;
-double currentAngle = 0.0;
+double initialAngle = 0.0;
+double currentAngle = 0.0; 
+double filteredAngle = 0.0; 
 double Q_angle = 0.001; 
 double Q_gyro = 0.003; 
 double R_measure = 0.03; 
@@ -23,39 +19,41 @@ double P[2][2] = {{1, 0}, {0, 1}};
 
 void setup() {
     Serial.begin(9600);  
-  
+    imu.begin();  
     imu.setGyroRange(MPU6050_RANGE_5_DEG);
-
     imu.setAccelerometerRange(MPU6050_RANGE_1G);
+}
 
-  }
-
-  // Set the gyroscope range to ±5 degrees per second
-
+void filterAngle(double newAngle, double newRate, double dt);
 
 void loop() {
-
     sensors_event_t accelerometerEvent, gyroscopeEvent;
     imu.getEvent(&accelerometerEvent, &gyroscopeEvent);
     double rawAngle = atan2(-accelerometerEvent.acceleration.y, -accelerometerEvent.acceleration.x) * RAD_TO_DEG;
 
     double dt = 0.1;
-    KalmanFilter(rawAngle, gyroscopeEvent.gyro.z, dt);  
+    filterAngle(rawAngle, gyroscopeEvent.gyro.z, dt);  
 
     if (currentAngle == 0.0) {
       currentAngle = rawAngle;
     }
+    
     //if (abs(currentAngle - rawAngle) > 5.0) {
-    //} //should run PID logic here in case the angle is veering more than MPU6050_RANGE_5_DEG
+        // Implement PID control logic here
+    //}
+
+    // Store filtered angle
+    filteredAngle = angle;
+    
     delay(100);
 }
 
-void KalmanFilter(double newAngle, double newRate, double dt) {
+void filterAngle(double newAngle, double newRate, double dt) {
     // Prediction update
     double rate = newRate - bias;
     angle += dt * rate;
 
-    // Update estimation error covariance - Project the error covariance ahead
+    // Update estimation error covariance 
     P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
     P[0][1] -= dt * P[1][1];
     P[1][0] -= dt * P[1][1];
